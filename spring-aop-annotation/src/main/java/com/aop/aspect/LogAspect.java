bbfad2d5daee87c50427927af9042e59063491f9
xiaonongOne <m13522519216@163.com>
2019-08-20 08:59:34 +0800

    切面类新增环绕方法

--- spring-aop-annotation/src/main/java/com/aop/aspect/LogAspect.java
+++ spring-aop-annotation/src/main/java/com/aop/aspect/LogAspect.java
@@ -3,7 +3,9 @@ package com.aop.aspect;
 import java.lang.reflect.Method;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -11,16 +13,16 @@ import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
 import com.aop.annotation.Action;
-
+ 
 @Aspect
 @Component
-
 public class LogAspect {
     @Pointcut("@annotation(com.aop.annotation.Action)")
     public void annotationPointCut() {
         System.out.println("annotationPointCut");
     }
     
+
     @After("annotationPointCut()")
     public void after(JoinPoint joinPoint) {
         MethodSignature signature = (MethodSignature)joinPoint.getSignature();
@@ -34,5 +36,14 @@ public class LogAspect {
         MethodSignature signature = (MethodSignature)joinPoint.getSignature();
         Method method = signature.getMethod();
         System.out.println("方法规则式拦截，" + method.getName());
+    }
+    
+    
+    @Around(value="execution(* com.aop.service.DemoMethodService.*(..))")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.println("环绕前通知======================");
+        Object obj=joinPoint.proceed();//执行目标方法
+        System.out.println("环绕后通知======================");
+        return obj;
     }
 }
